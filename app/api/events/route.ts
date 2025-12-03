ef51a7bb2ae206913a99faa2fcc41a7f4eb1eb90
Tochukwu <towunwanne@deloitte.com.ng>
2025-12-03 16:45:15 +0100

    implement api routes, server actions and caching, and the event details page

---
+++ app/api/events/route.ts
@@ -0,0 +1,73 @@
+import Event from "@/database/event.models";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+import { v2 as cloudinary } from 'cloudinary';
+
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const formData = await req.formData()
+
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
+        }
+
+        //get image file
+        const file = formData.get('image') as File;
+        if (!file) {
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 })
+        }
+
+        let tags = JSON.parse(formData.get('tags') as string);
+        let agenda = JSON.parse(formData.get('agenda') as string);
+        
+        //upload image to cloudinary
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvents'}, (error, result) => {
+                if (error) return reject(error);
+
+                resolve(result);
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+
+        const createdEvent = await Event.create({
+            ...event,
+            tags: tags,
+            agenda: agenda,
+        });
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 })
+        
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event creation failed', error: e instanceof Error ? e.message : 'Unknown' }, { status: 500})
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        //createdAt: -1 for descending order: new events first
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Event fetched successfully', events }, { status: 200 })
+        
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Failed to fetch event ', error: e }, { status: 500});
+    }
+}
+
---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import Event from "@/database/event.models";
+import connectDB from "../mongodb";
+
+export const  getSimlilarEventsBySlug = async(slug: string) => {
+    try{
+        await connectDB();
+
+        const event = await Event.findOne({ slug });
+        const similarEvents = await Event.find({_id: { $ne: event._id}, tags: { $in: event.tags}  }).lean();
+
+        return similarEvents;
+    } catch {
+        return [];
+    }
+}
