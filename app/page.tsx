f2129cc41e1db5e8353a3dcdd43ba05fef149970
Tochukwu <towunwanne@deloitte.com.ng>
2025-12-16 11:41:59 +0100

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,39 +3,34 @@ import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database/event.models";
 import { cacheLife } from "next/cache";
 
-
-
-
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const page = async () => {
-  'use cache'
-  cacheLife('hours')
-  const response = await fetch(`${BASE_URL}/api/events`)
-  const { events } = await response.json();
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Cannot Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-  return (
-    <section>
-      <h1 className="text-center">
-        The Hub for Amazing Events <br /> You Don&apos;t Want to Miss!
-      </h1>
-      <p className="text-center mt-5">Hackathons, Meetups and Conferences. You can find all here</p>
+            <ExploreBtn />
 
-      <ExploreBtn />
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
 
-      <div className="mt-7 space-y-7">
-        <h3>Featured Events</h3>
-        <ul className="events">
-{events && events.length > 0 && events.map((event:IEvent) => (
-  <li key={event.title} className="list-none">
-    <EventCard {...event} />
-  </li>
-))}
-        </ul>
-      </div>
-    </section>
-  )
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
-export default page
+export default Page;
